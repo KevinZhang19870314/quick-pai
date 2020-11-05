d238d00aeef8f0362c019df7917a2fb46a0580f2
kevin.zhang <kevinzhang19870314@gmail.com>
2020-11-05 19:20:54 +0800

    feat: wired radio-button control(partial)

--- projects/x-controls/src/lib/radio-button/radio-button.ts
+++ projects/x-controls/src/lib/radio-button/radio-button.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter, forwardRef, ViewEncapsulation, Inject, Directive, ContentChildren, QueryList, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, forwardRef, ViewEncapsulation, Inject, Directive, ContentChildren, QueryList, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { Point, XControl } from '../common/control';
 import { Utils } from '../common/utils';
+import { ellipse, svgNode } from '../common/wired-lib';
 
 @Component({
   selector: `x-radio-button`,
@@ -8,7 +10,7 @@ import { Utils } from '../common/utils';
   styleUrls: ['radio-button.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class XRadioButton implements OnInit {
+export class XRadioButton extends XControl implements OnInit {
 
   @Input() xValue: any;
   @Input() xDisabled: boolean = false;
@@ -17,7 +19,12 @@ export class XRadioButton implements OnInit {
   xColor: 'primary' | 'accent' | 'warn' = 'primary';
   xChecked: boolean = false;
 
-  constructor(@Inject(forwardRef(() => XRadioButtonGroup)) private radioButtonGroup: XRadioButtonGroup) {
+  private svgCheck?: SVGElement;
+  @ViewChild('svg', { static: true }) svg: ElementRef<SVGElement>;
+
+  constructor(@Inject(forwardRef(() => XRadioButtonGroup)) private radioButtonGroup: XRadioButtonGroup,
+    private el: ElementRef) {
+    super(el);
     if (!this.radioButtonGroup) {
       throw Error('radio button control must be included in radio button group');
     }
@@ -30,6 +37,10 @@ export class XRadioButton implements OnInit {
 
   ngOnChanges() {
 
+  }
+
+  ngAfterViewInit() {
+    this.render();
   }
 
   onChanged(event: Event) {
@@ -47,6 +58,25 @@ export class XRadioButton implements OnInit {
     // If current radio button is in checked state, prevent click itself to unchecked
     if (this.xChecked) {
       event.preventDefault();
+    }
+  }
+
+  protected canvasSize(): Point {
+    return [19, 19];
+  }
+
+  protected draw(svg: SVGElement, size: Point): void {
+    ellipse(svg, size[0] / 2, size[1] / 2, size[0], size[1], this.seed);
+    this.svgCheck = svgNode('g');
+    svg.appendChild(this.svgCheck);
+    const iw = Math.max(size[0] * 0.6, 5);
+    const ih = Math.max(size[1] * 0.6, 5);
+    ellipse(this.svgCheck, size[0] / 2, size[1] / 2, iw, ih, this.seed);
+  }
+
+  refreshCheckVisibility() {
+    if (this.svgCheck) {
+      this.svgCheck.style.display = this.xChecked ? '' : 'none';
     }
   }
 }
@@ -107,6 +137,7 @@ export class XRadioButtonGroup implements OnInit, ControlValueAccessor {
         }
 
         this.cdf.detectChanges();
+        radio.refreshCheckVisibility();
       });
     }
   }
